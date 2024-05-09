eb54d444db9ba8d15aac176846a97da4557d6a85
ninanee <ni.yun@outlook.com>
2024-05-08 22:06:11 -0400

    finished projecy

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -10,15 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var stories = StoryBrain()
+    
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
+             
+        
     }
 
-
+    @IBAction func choiceMade(_ sender: UIButton) {
+        stories.nextStory(userChoice: sender.currentTitle!)
+        updateUI()
+        
+    }
+    
+    func updateUI(){
+        storyLabel.text = stories.getStoryText()
+        choice1Button.setTitle(stories.getStoryChoice1(), for: .normal)
+        choice2Button.setTitle(stories.getStoryChoice2(), for: .normal)
+    }
+    
 }
 
--- Destini-iOS13/Model/StoryBrain.swift
+++ Destini-iOS13/Model/StoryBrain.swift
@@ -8,4 +8,39 @@
 
 import Foundation
 
+struct StoryBrain{
+    var storyNum = 0
+    
+    let stories = [
+        Story(title: "You see a fork in the road", choice1: "Take a left.", choice2: "Take a right", choice1Destination: 1,
+             choice2Destination: 2),
+        Story(title: "You see a tiger", choice1: "Shout for help.", choice2: "Play dead", choice1Destination: 0,
+             choice2Destination: 0),
+        Story(title: "You find a treasure chest", choice1: "Open it.", choice2: "Check for traps", choice1Destination: 0,
+             choice2Destination: 0)
+    ]
+    
+    func getStoryText() -> String {
+        return stories[storyNum].title
+    }
+    
+    func getStoryChoice1() -> String {
+        return stories[storyNum].choice1
+    }
+    
+    func getStoryChoice2() -> String {
+        return stories[storyNum].choice2
+    }
+    
+    mutating func nextStory(userChoice: String) {
+        let currentStory = stories[storyNum]
+        
+        if userChoice == currentStory.choice1 {
+            storyNum = currentStory.choice1Destination
+        } else if userChoice == currentStory.choice2 {
+            storyNum = currentStory.choice2Destination
+        }
+    }
+    
 
+}
